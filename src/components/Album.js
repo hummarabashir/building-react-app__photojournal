7154529002bc97a6a4851def2d089620e87ce72b
Hummara Bashir <bashirh@state.gov>
2022-10-28 16:18:32 -0400

    pj

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import PhotoEntry from './PhotoEntry';
+import entries from './data/photo-entries';
 
-export default function Album(){
-    return(
-        <div className="photo-entry-block">
+class Album extends React.Component {
+    render() {
+        return (
+            <div className = "flex-container">
+                {
+                    entries.itemlist.map((photo) => {
+                        return <PhotoEntry key={photo.src} src={photo.src} location={photo.location} caption={photo.caption} />
+                    }
+                    )
+                }
+            </div>
+        )
+
+        {/* <div className="photo-entry-block">
         <img className="photo-img" src="../images/photo1.png" />
         <img className="photo-img" src="../images/photo2.png" />
         <img className="photo-img" src="../images/photo3.png" />
-        {/* <img className="photo-img" src="../images/photo4.png" />
+        <img className="photo-img" src="../images/photo4.png" />
         <img className="photo-img" src="../images/photo5.png" />
-        <img className="photo-img" src="../images/photo6.png" /> */}
-        </div>
-    )
+        <img className="photo-img" src="../images/photo6.png" /> 
+        </div> */}
+    
 }
+}
+
+export default Album;
